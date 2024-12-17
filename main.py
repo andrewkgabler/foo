ca0849a53df83e0c4d8ff90c47d53bd29d3f94ff
Andrew Gabler <andrewkgabler@gmail.com>
2024-12-17 16:59:30 -0700

    call condition function in main

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from typing import List
 from mock import async_mock_fetch_telemetry_data, TelemetryData
 import logging
 
+from tests.test_clean_data import ConditionedTelemetry, condition_data
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,6 +14,8 @@ logger = logging.getLogger(__name__)
 # Example usage
 async def main():
     all_telemetry_data: List[TelemetryData] = await fetch_all_data()
+    conditioned_telemetry:ConditionedTelemetry=condition_data(all_telemetry_data)
+
 
 
 if __name__ == "__main__":
